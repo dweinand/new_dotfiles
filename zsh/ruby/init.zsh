0eb98e3d0b0f0c9592f74bd04f2484cff82570f9
Dan Weinand <dweinand@gmail.com>
2013-03-13 10:20:48 -0700

    new ruby aliases

--- zsh/ruby/init.zsh
+++ zsh/ruby/init.zsh
@@ -1,6 +1,5 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-alias rb="ruby"
-alias rk="rake"
-alias b="bundle"
+alias rake="noglob rake"
 alias be="bundle exec"
+alias fr="foreman run"

f078cfd0d722157b996a50d7de7211d9637839b3
Dan Weinand <dweinand@gmail.com>
2014-03-30 02:24:50 -0700

    fixes to make readlink work

--- zsh/homebrew/init.zsh
+++ zsh/homebrew/init.zsh
@@ -1,4 +1,5 @@
 if which brew > /dev/null; then
   export BREW_PREFIX=`brew --repository`
-  export PATH=$BREW_PREFIX/bin:$PATH
+  export PATH=$BREW_PREFIX/bin:$BREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH
+  export MANPATH=$BREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH
 fi
